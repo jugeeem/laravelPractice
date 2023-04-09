37f9c9081402c818d09c4b76cb3fd6c01d19516b
jugemu373 <1004traitor@gmail.com>
2023-04-09 17:58:49 +0900

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +20,19 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // アロー演算子を用いたルーティング
-Route::get('/hello_world', fn() =>view('hello_world'));
+Route::get('/hello_world', fn () => view('hello_world'));
 
 // 引数を用いたルーティング
-Route::get('/hello', fn() => view('hello',[
+Route::get('/hello', fn () => view('hello', [
     'name' => 'ミナミ',
     'course' => 'laravel'
 ]));
 
 // レイアウト
-Route::get('/', fn() => view('index'));
-Route::get('/curriculum', fn() => view('curriculum'));
+Route::get('/', fn () => view('index'));
+Route::get('/curriculum', fn () => view('curriculum'));
+
+// コントローラ
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
