139e76efb54b4fb7419de97d77e05caedb78bbc7
jugemu373 <1004traitor@gmail.com>
2023-04-09 23:24:03 +0900

    session - flash massage -

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,9 +2,13 @@
 
 @section('title', 'イベント登録')
 @section('content')
+    @if (session()->has('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
     <form action="{{ route('events.store') }}" method="post">
         @csrf
-				<label for="title">イベント名:<input type="text" name="title"></label>
-				<button type="submit">登録</button>
+        <label for="title">イベント名:<input type="text" name="title"></label>
+        <button type="submit">登録</button>
     </form>
 @endsection
